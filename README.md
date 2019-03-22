88232595121dd8ab79289bd473bf9f3aecec40c0
AndyGFX <o.kollar@gmail.com>
2019-03-22 20:33:47 +0100

    Finalize TileSet Builder plugin

---
+++ README.md
@@ -0,0 +1,21 @@
+# Godot 3.1 Tileset Builder plugin
+Create 3x3 MINIMAL tileset from image:
+
+
+- Screenshot:
+
+![Alt text](Screenshots/Godot_v3.1-stable_win64_2019-03-22_20-28-46.png?raw=true "PREVIEW")
+
+
+
+# Usage:
+
+- Copy folder TilsetBuilder_Plugin to your project inside addnos folder
+- Open Project setting/Plugins and Activate TilsetBuilder_Plugin
+
+# Help
+
+- [+] = Add PNG file to list
+- [-] = Remove selected item from list
+- [C] = Clear list
+- [Export Tileset] = Generate and save tileset to files as resource [tres]
--- addons/TilsetBuilder_Plugin/TilesetBuilderDock.gd
+++ addons/TilsetBuilder_Plugin/TilesetBuilderDock.gd
@@ -1,34 +1,50 @@
 tool
-extends Panel
+extends Control
+
 var editor_interface
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 var items_list = null
 var selected_item_id = -1
+var src_images = {}
+var Builder = TilesetBuilder.new()
 
-# Called when the node enters the scene tree for the first time.
+class TileItem:
+	var width:int = 16
+	var height:int = 16
+	var name:String = "noname"
+	var src:Image = null
+
+
+
 func _ready():
 	get_node("Button_ADD").connect("pressed", self, "ADD_pressed")
 	get_node("Button_DEL").connect("pressed", self, "DEL_pressed")
+	get_node("Button_CLEAR").connect("pressed", self, "CLEAR_pressed")
+	get_node("Button_EXP").connect("pressed", self, "EXP_pressed")
 	self.items_list = get_node("ItemList")
 	get_node("ItemList").connect("item_selected",self,"SelectedItemId")
 	get_node("FileDialog_SAVE").connect("file_selected", self, "SAVE_file")
 	get_node("FileDialog_OPEN_PNG").connect("file_selected", self, "OPEN_PNG_file")
 	
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass 
 
 func SelectedItemId(ID:int):
 	self.selected_item_id = ID
 	pass
 
+func CLEAR_pressed():
+	print("clear images ...")
+	self.items_list.clear()
+	pass
+	
 func ADD_pressed():
 	print("select image ...")
 	get_node("FileDialog_OPEN_PNG").popup_centered()
+	
+func EXP_pressed():
+	get_node("FileDialog_SAVE").popup_centered()
 	
 func DEL_pressed():
 	if (self.items_list.get_item_count()>0):
@@ -36,14 +52,25 @@ func DEL_pressed():
 			self.items_list.remove_item(self.selected_item_id)
 	
 	
-func SAVE_file():
+func SAVE_file(path):
+	Builder.BuildFromImages(src_images,path)
 	pass
 	
 func OPEN_PNG_file(path):
-	var img = Image.new()
-	img.load(path) 
 	
 	var split_path = path.split("/")
 	var filename = split_path[split_path.size()-1]
-	items_list.add_item(filename,load(path))
+	var icon:Texture = load(path)
+	items_list.add_item(filename,icon)
+	
+	var id = items_list.get_item_count()-1
+	var src_item:TileItem = TileItem.new()
+	src_item.width = int(get_node("LineEdit_w").text)
+	src_item.height = int(get_node("LineEdit_h").text)
+	src_item.name = filename	
+	src_item.src = Image.new()
+	src_item.src = icon.get_data()
+	
+	src_images[String(id)] = src_item
+	
 	pass	
