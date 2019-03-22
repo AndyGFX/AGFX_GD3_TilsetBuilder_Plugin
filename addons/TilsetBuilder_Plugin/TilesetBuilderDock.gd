8655bf00b574e0f1aae8aa91d49b1d6d26438b5a
AndyGFX <o.kollar@gmail.com>
2019-03-22 13:01:10 +0100

    extract png name from path and added DEL selected img from list

--- addons/TilsetBuilder_Plugin/TilesetBuilderDock.gd
+++ addons/TilsetBuilder_Plugin/TilesetBuilderDock.gd
@@ -5,12 +5,14 @@ var editor_interface
 # var a = 2
 # var b = "text"
 var items_list = null
+var selected_item_id = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("Button_ADD").connect("pressed", self, "ADD_pressed")
 	get_node("Button_DEL").connect("pressed", self, "DEL_pressed")
 	self.items_list = get_node("ItemList")
+	get_node("ItemList").connect("item_selected",self,"SelectedItemId")
 	get_node("FileDialog_SAVE").connect("file_selected", self, "SAVE_file")
 	get_node("FileDialog_OPEN_PNG").connect("file_selected", self, "OPEN_PNG_file")
 	
@@ -20,14 +22,19 @@ func _ready():
 #func _process(delta):
 #	pass
 
-
+func SelectedItemId(ID:int):
+	self.selected_item_id = ID
+	pass
 
 func ADD_pressed():
 	print("select image ...")
 	get_node("FileDialog_OPEN_PNG").popup_centered()
 	
 func DEL_pressed():
-	pass
+	if (self.items_list.get_item_count()>0):
+		if (self.selected_item_id>=0):
+			self.items_list.remove_item(self.selected_item_id)
+	
 	
 func SAVE_file():
 	pass
@@ -35,5 +42,8 @@ func SAVE_file():
 func OPEN_PNG_file(path):
 	var img = Image.new()
 	img.load(path) 
-	items_list.add_item(path,load(path))
+	
+	var split_path = path.split("/")
+	var filename = split_path[split_path.size()-1]
+	items_list.add_item(filename,load(path))
 	pass	
